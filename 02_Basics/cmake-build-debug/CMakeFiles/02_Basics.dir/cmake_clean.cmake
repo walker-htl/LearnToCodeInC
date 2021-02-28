227754d785aad8ee55ab7dcc4f996aef868a35b5
walker99 <www.kerber@gmail.com>
2021-02-28 18:53:25 +0100

    Add as cmake project

---
+++ 02_Basics/cmake-build-debug/CMakeFiles/02_Basics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "02_Basics.exe"
+  "02_Basics.exe.manifest"
+  "02_Basics.pdb"
+  "CMakeFiles/02_Basics.dir/main.c.obj"
+  "lib02_Basics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/02_Basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
