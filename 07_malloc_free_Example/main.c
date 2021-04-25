9d4bd267a16af740c7f6726036b859952daf8ea8
Walter Kerber <Walter.Kerber@htl-wels.at>
2021-04-25 20:44:18 +0200

    How large can a array be on your machine?

---
+++ 07_malloc_free_Example/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *createArray(unsigned int n);
+
+int main() {
+    int *arr = NULL;
+    unsigned int val=0, i=0;
+
+    // 8 GB Linux Rechner:
+    // 6 GB konnten allokiert werden 1.500.000.000 Elemente
+    // 8 GB konnten nicht allokiert werden 2.000.000.000 Elemente
+    printf ("Wie viele int-Elemente benötigen Sie? ");
+    fflush(stdout);
+    scanf("%u", &val);
+    arr = createArray(val);
+
+    if (arr==NULL) {
+        printf ("nicht genügend speicher");
+        return -1;
+    }
+
+    printf ("Ausgabe der Elemente\n");
+
+    for (i=0;i<val;i++) {
+        printf ("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    free(arr);
+    arr=NULL;
+
+    return 0;
+}
+
+// reserviert einen Speicherbereich für n-INT Werte
+// und gibt einen Zeiger darauf zurück
+int *createArray(unsigned int n) {
+    int *iptr = NULL;
+    unsigned int i = 0;
+    iptr = malloc (n*(sizeof(int)));
+
+    if (iptr!=NULL) {
+        for (i = 0; i < n; i++) {
+            iptr[i] = i; // *(iptr+i) = ...;
+        }
+    }
+
+    return iptr;
+}
